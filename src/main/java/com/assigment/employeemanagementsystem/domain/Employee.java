83e9a85046a3e47aa462ee4d490dc689483b6970
HCanUgur <can.ugur725@gmail.com>
2022-07-12 17:55:04 +0300

    Used new template

---
+++ src/main/java/com/assigment/employeemanagementsystem/domain/Employee.java
@@ -0,0 +1,63 @@
+package com.assigment.employeemanagementsystem.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to create a table of this class
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//	By @GeneratedValue, JPA makes a unique key automatically and applies the key to the field having @Id
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String dep;
+
+	private Long salary;
+
+	public Employee() {
+	}
+
+	public Long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDep() {
+		return dep;
+	}
+
+	public void setEmail(String email) {
+		this.dep = dep;
+	}
+
+}
---
+++ src/main/java/com/assigment/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.assigment.employeemanagementsystem.repository;
+
+import com.assigment.employeemanagementsystem.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+	// This will be AUTO IMPLEMENTED by Spring into a Bean
+}
